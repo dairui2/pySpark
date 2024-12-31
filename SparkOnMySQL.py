ff3c9fed44f601f3de61517ce230c73d2a793f9f
dairui3 <dairui3@163.com>
2025-01-01 06:56:35 +0800

    SparkOnMySQL

--- SparkOnMySQL.py
+++ SparkOnMySQL.py
@@ -3,6 +3,6 @@ from pyspark.sql import SparkSession
 #➜  ~ vi ./.bash_profile
 # export HADOOP_USER_NAME=hive
 spark = SparkSession.builder.appName("SparkOnMySQL").enableHiveSupport().getOrCreate()
-spark.sql("create table db1.spark_on_mysql2(id int, word varchar(20))")
+spark.sql("create table db1.spark_on_mysql(id int, word varchar(20))")
 spark.sql("insert into db1.spark_on_mysql values(3, 'Spark')")
 spark.sql("select * from db1.spark_on_mysql").show()
